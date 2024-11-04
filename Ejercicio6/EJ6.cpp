1749b7d7dab79b589ae3f7c00add501fd7ccd93f
junx21 <jxuche@myuax.com>
2024-11-04 17:24:30 +0100

    Ej6

---
+++ Ejercicio6/EJ6.cpp
@@ -0,0 +1,28 @@
+#include "EJ6.h"
+#include <iostream>
+
+using namespace std;
+
+int puntero() {
+    int numero = 10;
+    int* puntero = &numero;
+
+    cout << "Valor de numero: " << numero << endl;
+    cout << "Dirección de numero: " << &numero << endl;
+    cout << "Valor de puntero: " << puntero << endl;
+    cout << "Valor apuntado por puntero: " << *puntero << endl;
+
+    int* arregloDinamico = new int[5];
+    for(int i = 0; i < 5; i++) {
+        arregloDinamico[i] = i * 2;
+    }
+
+    cout << "\nValores del arreglo dinámico:" << endl;
+    for(int i = 0; i < 5; i++) {
+        cout << arregloDinamico[i] << " ";
+    }
+
+    delete[] arregloDinamico;
+
+    return 0;
+}
---
+++ Ejercicio6/EJ6.h
@@ -0,0 +1,10 @@
+#ifndef EJ6_H
+#define EJ6_H
+
+
+
+int puntero();
+
+
+
+#endif //EJ6_H
