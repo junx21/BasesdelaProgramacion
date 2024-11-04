057b7cc5de17fa0a7f1865de3084453f47b0cdb2
junx21 <jxuche@myuax.com>
2024-11-04 17:24:20 +0100

    Ej4

--- Ejercicio4/EJ4.cpp
+++ Ejercicio4/EJ4.cpp
@@ -8,10 +8,7 @@ int array(){
 
     for (int i = 0; i < 10; i++) {
         numeros[i] = i + 1;
-    }
-    for (int i = 0; i < 10; i++) {
         cout << numeros[i] << endl;
     }
-
     return 0;
 }
